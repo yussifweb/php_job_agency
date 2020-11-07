5fc95f68175e02501b0453e71d8820808f0a2045
Yussif <djiceman.gh@gmail.com>
2020-11-07 22:24:37 +0000

    added image extensions &/ form changes

--- company.php
+++ company.php
@@ -53,7 +53,7 @@ if( !$_SESSION['email'] ){
                     <input type="tel" class="form-control" id="phone" min="10" name="phone" placeholder="Your Contact Here..." required>
                 </div>
 
-                <div class="form-group">
+                <!-- <div class="form-group">
                   <label for="industry">Industry</label>
                   <select class="form-control" name="industry" id="dropdown">
                       <option selected>Select</option>
@@ -64,9 +64,13 @@ if( !$_SESSION['email'] ){
                       <option>Food</option>
                       <option>Other</option>
                   </select>
-              </div>
+              </div>--> 
+                   <div class="form-group">
+                    <label for="industry" id="industry-label">Industry</label>
+                    <input type="industry" class="form-control" id="industry" name="industry" placeholder="Please Enter Your Industry" required>
+                </div>
 
-                <div class="form-group">
+                <!-- <div class="form-group">
                   <label for="region">Region</label>
                   <select class="form-control" id="dropdown" name="region">
                       <option selected>Select</option>
@@ -76,9 +80,15 @@ if( !$_SESSION['email'] ){
                       <option>Brong Ahafo</option>
                       <option>Eastern</option>
                   </select>
-              </div>
+              </div> -->
 
-            <div class="form-group">
+              <div class="form-group">
+                    <label for="region" id="region-label">Region</label>
+                    <input type="region" class="form-control" id="region" name="region" placeholder="Please Enter Your Region" required>
+                </div>
+
+
+            <!-- <div class="form-group">
               <label for="district">District</label>
               <select class="form-control" id="dropdown" name="district">
                   <option selected>Select</option>
@@ -88,8 +98,12 @@ if( !$_SESSION['email'] ){
                   <option>Nhyiaeso Municipal</option>
                   <option>Eastern</option>
               </select>
-          </div>
-          
+          </div> -->
+
+          <div class="form-group">
+                    <label for="district" id="district-label">District</label>
+                    <input type="district" class="form-control" id="district" name="district" placeholder="Please Enter Your District" required>
+          </div>          
           
           <?php 
             $email = $_SESSION['email'];
@@ -145,7 +159,7 @@ if( !$_SESSION['email'] ){
         $width = $info[0];
         $height = $info[1];
 
-        $allowed_extension = array( "png", "jpg", "jpeg" );
+        $allowed_extension = array( "png", "PNG", "JPG", "jpg", "jpeg", "JPEG" );
 
         // validate image size. Size is calculated in Bytes
         if($_FILES['image']['size'] > 300000) {
@@ -165,7 +179,7 @@ if( !$_SESSION['email'] ){
         
         if (! in_array($extension, $allowed_extension)) {  
             $error ='unsupported extension';
-            $msg = "Image should be jpg, png or jpeg";
+            $msg = "Image should be JPG, jpg, png, PNG, JPEG or jpeg";
             $msg_class = "alert-danger";
             // return false;
          }
